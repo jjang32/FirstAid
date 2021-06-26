ef8538da0c23f5b913112720ae504df29de2e729
nyu12345 <nathan.yu@duke.edu>
2021-06-26 00:38:32 -0400

    EchoAR addition

--- ARPhotoViewerDemo/EchoAR.swift
+++ ARPhotoViewerDemo/EchoAR.swift
@@ -19,7 +19,7 @@ import Foundation
 
 
 class EchoAR{
-    let api_key="shrill-bird-7026" //insert your echoAR API key here
+    let api_key="billowing-cloud-7438" //insert your echoAR API key here
     //echoar endpoint
     var serverURL="https://console.echoar.xyz/query?key="
     //websocket endpoint
