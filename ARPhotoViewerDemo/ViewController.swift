379959fb75559d1a84e75dbd75248e053e10024b
Richard Deng <rld39@duke.edu>
2021-06-26 17:30:35 -0700

    call me maybe

--- ARPhotoViewerDemo/ViewController.swift
+++ ARPhotoViewerDemo/ViewController.swift
@@ -73,11 +73,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak alert] (_) in
                     guard let textField = alert?.textFields?[0], let userText = textField.text else { return }
                     print("User text: \(userText)")
+                if (userText.lowercased() == "broken arm" ||                        userText.lowercased() == "broken leg" ||
+                        userText.lowercased() == "gunshot wound" ||
+                        userText.lowercased() == "stab wound" ||
+                        userText.lowercased() == "i'm dying") {
+                            self.callNumber(phoneNumber: "2407071760")
+                    }
                     self.injuryLabel.text = userText
                     self.injuryLabel.isHidden = false
                 }))
         
         self.present(alert, animated: true, completion: nil)
+    }
+        
+    private func callNumber(phoneNumber:String) {
+        if let phoneCallURL = URL(string: "tel://\(phoneNumber)") {
+            let application:UIApplication = UIApplication.shared
+            if (application.canOpenURL(phoneCallURL)) {
+                application.open(phoneCallURL, options: [:], completionHandler: nil)
+            }
+        }
     }
     
     @IBAction func addModel(_ sender: Any) {
